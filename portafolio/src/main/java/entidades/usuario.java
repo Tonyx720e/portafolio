21ff79f4143d237f4440d6b1f14bf2177b0aa713
Tony Medina <cayapadeveloper@gmail.com>
2024-11-18 23:44:38 +0100

    crear entidad usuario

--- portafolio/src/main/java/entidades/usuario.java
+++ portafolio/src/main/java/entidades/usuario.java
@@ -9,5 +9,47 @@ package entidades;
  * @author tonym
  */
 public class usuario {
+    private int idusuario;
+    private String nombre;
+    private String apellido;
+
+    public usuario(int idusuario, String nombre, String apellido) {
+        this.idusuario = idusuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public usuario(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public usuario() {
+    }
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
     
 }
